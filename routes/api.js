430d058e21f70e12f57068fe3eb59787a041f084
Peter Batley <peter.batley@gmail.com>
2017-07-21 11:38:28 +0100

    fix api.js

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //const conf = require('./conf.js');
 
 // Connect to mongodb
-mongoose.connect('mongodb://localhost:27017/local-test-data');
+mongoose.connect('mongodb://database:27017');
 
 // create mongoose schema
 const userSchema = new mongoose.Schema({
